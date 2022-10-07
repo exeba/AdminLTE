0fd07d97a60d60e549898ba5074b9dccfb76da2b
Sebastiano Degan <sebastiano@localhost.localdomain>
2022-10-07 11:22:36 +0200

    fixed mem & cpu stats

--- scripts/pi-hole/php/func.php
+++ scripts/pi-hole/php/func.php
@@ -16,6 +16,64 @@ function getPiholeFilePath($relativePath)
     return "$piholeDir/$relativePath";
 }
 
+// Return memory usage to show on status block
+function getMemUsage()
+{
+    $meminfo = array();
+    if (file_exists("/proc/meminfo"))
+    {
+        // Linux
+        $data = explode("\n", file_get_contents("/proc/meminfo"));
+        if(count($data) > 0)
+        {
+            foreach ($data as $line) {
+                $expl = explode(":", trim($line));
+                if(count($expl) == 2)
+                {
+                    // remove " kB" from the end of the string and make it an integer
+                    $meminfo[$expl[0]] = intval(substr($expl[1],0, -3));
+                }
+            }
+            $memory_used = $meminfo["MemTotal"]-$meminfo["MemFree"]-$meminfo["Buffers"]-$meminfo["Cached"];
+            $memory_total = $meminfo["MemTotal"];
+        }
+    }
+    else
+    {
+        // FreeBSD
+        // Taken from 'sys/compat/linprocfs/linprocfs.c'
+        $sysctl_output = shell_exec('sysctl '.join(' ',[
+            'hw.physmem',
+            'vm.stats.vm.v_free_count',
+            'vm.stats.vm.v_active_count',
+            'vm.stats.vm.v_inactive_count',
+            'vm.stats.vm.v_laundry_count',
+            'vfs.bufspace',
+        ]));
+        $data = explode("\n", $sysctl_output);
+        if(count($data) > 0)
+        {
+            foreach ($data as $line) {
+                $expl = explode(":", trim($line));
+                if(count($expl) == 2)
+                {
+                    // remove " kB" from the end of the string and make it an integer
+                    $meminfo[$expl[0]] = intval($expl[1]);
+                }
+            }
+            $memory_used = $meminfo["hw.physmem"]
+                -$meminfo["vm.stats.vm.v_free_count"]
+                -$meminfo["vfs.bufspace"]
+                -$meminfo["vm.stats.vm.v_active_count"]
+                -$meminfo["vm.stats.vm.v_inactive_count"]
+                -$meminfo["vm.stats.vm.v_laundry_count"];
+            $memory_total = $meminfo["hw.physmem"];
+        }
+    }
+
+    return $meminfo ? $memory_used/$memory_total : -1;
+}
+
 // Credit: http://stackoverflow.com/a/4694816/2087442
 // Modified because of https://github.com/pi-hole/AdminLTE/pull/533
 ini_set('pcre.recursion_limit', 1500);
